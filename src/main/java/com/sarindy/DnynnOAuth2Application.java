eb4bbc8b49fb2ba83c122b760f03808fe727365e
sarindy <sarindyo@yahoo.com>
2017-05-05 16:12:12 +0700

    1rst

---
+++ src/main/java/com/sarindy/DnynnOAuth2Application.java
@@ -0,0 +1,12 @@
+package com.sarindy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnynnOAuth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DnynnOAuth2Application.class, args);
+	}
+}
